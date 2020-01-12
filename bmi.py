a0059caf66de5539467183d93f93096353f11785
Ivan Rubinson <soryy708@gmail.com>
2020-01-12 21:08:14 +0200

    Implement calculateBmi

--- bmi.py
+++ bmi.py
@@ -1,5 +1,11 @@
+
 def calculateBmi(mass, height):
     """
         Calculates Body Mass Index from `mass` and `height` (numbers) and returns it
     """
-    return 0
+    if mass < 0:
+        raise ValueError('mass < 0')
+    if height <= 0:
+        raise ValueError('height <= 0')
+
+    return mass / (height ** 2)
