05d17b4484c460eabaa36015e7f3c03428b00953
Ivan Rubinson <soryy708@gmail.com>
2020-01-12 21:41:16 +0200

    Implement bblSort

--- bblsort.py
+++ bblsort.py
@@ -1,3 +1,11 @@
 
 def bblSort(tpl):
-    return tuple()
+    """
+        Given unsorted tuple `tpl`, return a sorted copy of `tpl`, using bubble sort (O(n^2) complexity)
+    """
+    lst = list(tpl)
+    for i in range(len(lst) - 1, 0, -1):
+        for j in range(i):
+            if lst[j] > lst[j + 1]:
+                lst[j], lst[j+1] = lst[j+1], lst[j]
+    return tuple(lst)
